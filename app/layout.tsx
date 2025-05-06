60aff150d76c2e1b322e258c41967b6c51123513
Jonas09law <101458317+Jonas09law@users.noreply.github.com>
2025-05-06 22:32:58 +0100

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "Marcelo Portfolio",
+  title: "Marcelo Souza - Portfolio",
   description: "My Personal Portfolio <3"
 }
 
