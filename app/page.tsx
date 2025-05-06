a52cec6f4ae523ed5868a128f5934de96d6e8528
Jonas09law <101458317+Jonas09law@users.noreply.github.com>
2025-05-06 22:41:59 +0100

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -141,7 +141,7 @@ export default function Home() {
                       <path d="M12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2Z" />
                     </svg>
                   </div>
-                  <span className="text-xs text-[#4a4a46]/80">Discord Atividade</span>
+                  <span className="text-xs text-[#4a4a46]/80">Discord Atividades</span>
                 </div>
                 {lanyard ? (
                   lanyard.activities && lanyard.activities.length > 0 ? (
